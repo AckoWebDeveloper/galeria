e911c4cb5b8963016d95e7fa73a779aaea40050c
AckoWebDeveloper <97264590+AckoWebDeveloper@users.noreply.github.com>
2022-11-07 22:05:49 -0600

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Slider from './components/slide/Slider';
+
+import Sound from './components/sound/Sound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div class='sound'><><Sound /></></div>
+      <header className="App-header">     
+      <Slider />
       </header>
-    </div>
+  </div>
+        
   );
 }
 
